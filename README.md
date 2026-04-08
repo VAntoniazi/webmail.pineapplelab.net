ef2e0a6b50bda66f2f0b31fa91ef24d52f1db7d5
victor.gonzalez <99286703+VAntoniazi@users.noreply.github.com>
2026-04-08 15:15:47 -0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# webmail.pineapplelab.net
--- scripts/backup.sh
+++
@@ -1,7 +0,0 @@
-VOL="poste_data"
-DEST="/srv/backups/mail"
-TS="$(date +%F_%H-%M-%S)"
-mkdir -p "$DEST"
-
-docker run --rm -v ${VOL}:/v:ro -v ${DEST}:/b alpine \
-  sh -lc "cd /v && tar -czf /b/${VOL}_${TS}.tar.gz ."
